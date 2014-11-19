366f0436d25837b6ff82b69b1a091916fe7b7a98
YeTing <ting_ye@dnss-xa.denso.com.cn>
2014-11-19 10:56:37 +0800

    UI线：加入提示框(类似messageBox)

---
+++ CConverter/Prompt.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace CConverter
+{
+	partial class Prompt
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnOk = new System.Windows.Forms.Button();
+			this.lblHint = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnOk
+			// 
+			this.btnOk.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.btnOk.Location = new System.Drawing.Point(147, 68);
+			this.btnOk.Name = "btnOk";
+			this.btnOk.Size = new System.Drawing.Size(75, 23);
+			this.btnOk.TabIndex = 0;
+			this.btnOk.Text = "OK";
+			this.btnOk.UseVisualStyleBackColor = true;
+			this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+			// 
+			// lblHint
+			// 
+			this.lblHint.AutoSize = true;
+			this.lblHint.Location = new System.Drawing.Point(13, 13);
+			this.lblHint.Name = "lblHint";
+			this.lblHint.Size = new System.Drawing.Size(29, 12);
+			this.lblHint.TabIndex = 1;
+			this.lblHint.Text = "Hint";
+			// 
+			// Prompt
+			// 
+			this.AcceptButton = this.btnOk;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.btnOk;
+			this.ClientSize = new System.Drawing.Size(234, 103);
+			this.Controls.Add(this.lblHint);
+			this.Controls.Add(this.btnOk);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Prompt";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Prompt";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnOk;
+		private System.Windows.Forms.Label lblHint;
+	}
+}
---
+++ CConverter/Prompt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CConverter
+{
+	public partial class Prompt : Form
+	{
+		public Prompt()
+		{
+			InitializeComponent();
+		}
+
+		public Prompt(string text, string caption)
+		{
+			InitializeComponent();
+
+			this.lblHint.Text = text;
+			this.Text = caption;
+		}
+
+		private void btnOk_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
