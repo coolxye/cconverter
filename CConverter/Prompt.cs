20a5ce46a7879a147814e2f0d5a30f2aa8cea2a9
YeTing <ting_ye@dnss-xa.denso.com.cn>
2014-11-20 11:27:47 +0800

    提示框加入时间计数，并自动消失

--- CConverter/Prompt.cs
+++ CConverter/Prompt.cs
@@ -16,16 +16,30 @@ namespace CConverter
 			InitializeComponent();
 		}
 
+		private Timer tmPrompt;
+
 		public Prompt(string text, string caption)
 		{
 			InitializeComponent();
 
 			this.lblHint.Text = text;
 			this.Text = caption;
+
+			tmPrompt = new Timer();
+			tmPrompt.Interval = 800;
+			tmPrompt.Tick += new EventHandler(this.tmPrompt_Tick);
+			tmPrompt.Start();
 		}
 
 		private void btnOk_Click(object sender, EventArgs e)
 		{
+			this.tmPrompt.Stop();
+			this.Close();
+		}
+
+		private void tmPrompt_Tick(object sender, EventArgs e)
+		{
+			this.tmPrompt.Stop();
 			this.Close();
 		}
 	}
